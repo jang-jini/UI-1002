8ffae6fbdac6da21b325bad4a502e89d4445fecb
jangjini <wkdwls060523@naver.com>
2024-10-02 13:57:26 +0900

    구현완료

--- 01-GNB/01/js/script.js
+++ 01-GNB/01/js/script.js
@@ -2,14 +2,17 @@ $(function () {
     // 대상을 변수에 저장
     const $menu = $(".gnb > li");
     const $submenu = $(".submenu");
+    const duration = 300;
 
     // 메뉴에 마우스가 들어왔을때
     $menu.on("mouseenter", function () {
-        $submenu.slideDown();
+        // stop() : 현재 진행 중인 애니메이션 즉시 중지
+        $(this).find($submenu).stop().slideDown(duration);
+        $(this).addClass9("on");
     });
 
     // 메뉴에 마우스가 나갔을떄
     $menu.on("mouseleave", function () {
-        $submenu.slideUp();
+        $submenu.stop().slideUp(duration);
     });
 });
